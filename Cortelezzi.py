068d65d3e0c07a8c7bb83620fa53ff9a89293a44
ClementCR <77625450+ClementCR@users.noreply.github.com>
2021-04-19 10:50:10 +0200

    Update Cortelezzi.py with new functions and Rayleigh Taylor

--- Cortelezzi.py
+++ Cortelezzi.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#%%
 ##Import 
 
 import numpy as np
@@ -12,7 +11,6 @@ M_value = 32
 
 import matplotlib.pyplot as plt
 
-#%%
 ## Function and expression declarations
 #Declare the expressions of the kernel, A and eta
 def ker_sy (s, Oh, Bo, k, lbda):
@@ -42,14 +40,30 @@ def vortiLaplace(s, z, Oh, Bo, k):
 
 #Inverse the Laplace transfrom and return functions of t and of the parameters Oh, Bo and k
 def freeSurface(t, Oh, Bo, k):
-    return gwr(lambda s: freeSurfaceLaplace(s, Oh, Bo, k), t, M = 32)
+    return gwr(lambda s: freeSurfaceLaplace(s, Oh, Bo, k), t, M_value)
 
 def vorti(t, z, Oh, Bo, k):
-    return gwr(lambda s: vortiLaplace(s, z, Oh, Bo, k), t, M = 32)
+    return gwr(lambda s: vortiLaplace(s, z, Oh, Bo, k), t, M_value)
+    
 
-#%%
+#Parameters figures
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = [r'\usepackage[squaren,Gray]{SIunits}',
+                                       r'\usepackage{nicefrac}']
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = 'cm'
+#font size
+plt.rc('font', size=10)  # general font size
+plt.rc('axes', labelsize=10, titlesize=10)
+plt.rc('lines', markersize=8, markeredgewidth=0., linewidth=0.4)
+plt.rc('legend', frameon=False, fancybox=False, numpoints=1, markerscale=1, 
+       fontsize=10, handlelength=0.6, handletextpad=0.6, labelspacing=0.3)
+plt.rc('xtick',  labelsize=8, direction='in', bottom='true', top='true')
+plt.rc('ytick',  labelsize=8, direction='in', left='true', right='true')
+plt.rc('savefig', bbox='tight', transparent=True, dpi=300)    
+
 ##Main function
-def plotComparison(Ohnumb, Bonumb, knumb, nbOfrelax):
+def solveEta(Ohnumb, Bonumb, knumb, nbOfrelax):
     Oh = mp.mpmathify(Ohnumb)
     Bo = mp.mpmathify(Bonumb)
     k = mp.mpmathify(knumb) 
@@ -57,49 +71,57 @@ def plotComparison(Ohnumb, Bonumb, knumb, nbOfrelax):
     #Resolution functions with numerical Laplace Transform GWR. Oh_, Bo_ and k_ must have been defined before
     def eta(t):
         return float(freeSurface(t, Oh, Bo, k))
+       
+    #Time resolutions
+    tau_relax = float(3*(Oh/(k**2*Bo+k**4)))
+    t_all = np.linspace(0.01, 1., 200) * nbOfrelax * abs(tau_relax)
+    
+    #solve the equation on eta with Cortelezzi model and lubrication
+    sampled_t = abs(t_all/tau_relax)
+    sampled_eta = [abs(eta(t)) for t in t_all]
+    sampled_eta_lub = [np.exp(-t/tau_relax) for t in t_all]
+    
+    return sampled_t, sampled_eta, sampled_eta_lub
+
+def solveOmega(Ohnumb, Bonumb, knumb, nbOfrelax, toI, z_all):
+    Oh = mp.mpmathify(Ohnumb)
+    Bo = mp.mpmathify(Bonumb)
+    k = mp.mpmathify(knumb) 
     
     def omega(t, z_):    
         return float(vorti(t, z_, Oh, Bo, k))
-    
+       
+    #Spatial and time resolutions
+    tau_relax = abs(float(3*(Oh/(k**2*Bo+k**4))))
+    t_all = np.linspace(0.01, 1., 200) * nbOfrelax * tau_relax
+    timesOfInterest = np.array(toI)*tau_relax
+
     #determine the maximal value of vorticity
-    t_all = np.linspace(0.03, 3., 100)/Oh
-    z_top = mp.mpmathify('-0.99')
-    vortmax = [abs(omega(t, z_top)) for t in t_all]
+    vortmax = [abs(omega(t, mp.mpmathify(z_all[0]))) for t in t_all]
     maxv = max(np.array(vortmax))
     
-    #Spatial and time resolutions
-    tau_relax = float(3*(Oh/(k**2*Bo+k**4)))
-    timesOfInterest = np.array([0.1, 0.2, 0.5, 1])*nbOfrelax*tau_relax
-    z_all = np.linspace(-1, 0, 100)
-    t_all_eta = np.linspace(0.01, nbOfrelax, 100) * tau_relax
-    
     #solve the equation on omega with Cortelezzi model and lubrication
-    sampled_omega = np.array([[2*i+omega(timesOfInterest[i], z)/maxv
-        for i in range(4)] for z in z_all])
+    sampled_omega = np.array([[2*i+omega(timesOfInterest[i], mp.mpmathify(z))/maxv
+        for i in range(len(timesOfInterest))] for z in z_all])
     sampled_omega_lub = np.array([[2*i+float(k**3/Oh+k*Bo/Oh)*(-z)
         *np.exp(-timesOfInterest[i]/tau_relax)/maxv
-        for i in range(4)] for z in z_all])
-    sampled_t = t_all_eta/tau_relax
-    sampled_eta = [eta(t) for t in t_all_eta]
-    sampled_eta_lub = [np.exp(-t/tau_relax) for t in t_all_eta]
+        for i in range(len(timesOfInterest))] for z in z_all])
+    
+    return sampled_omega, sampled_omega_lub
+
+
+def plotComparison(Ohnumb, Bonumb, knumb, nbOfrelax, toI = [0.05, 0.1]):
+    
+    sampled_t, sampled_eta, sampled_eta_lub = \
+        solveEta(Ohnumb, Bonumb, knumb, nbOfrelax)
+    
+    z_all = np.linspace(-1, 0, 200)
+    sampled_omega, sampled_omega_lub = \
+        solveOmega(Ohnumb, Bonumb, knumb, nbOfrelax, toI, z_all)
     
     ### Figures ###
-    plt.rcParams['text.usetex'] = True
-    plt.rcParams['text.latex.preamble'] = [r'\usepackage[squaren,Gray]{SIunits}',
-                                           r'\usepackage{nicefrac}']
-    plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.serif'] = 'cm'
-    #font size
-    plt.rc('font', size=28)  # general font size
-    plt.rc('axes', labelsize=28, titlesize=28)
-    plt.rc('lines', markersize=9, markeredgewidth=0., linewidth=1.5)
-    plt.rc('legend', frameon=False, fancybox=False, numpoints=1, markerscale=1, 
-           fontsize=16, handlelength=0.6, handletextpad=0.6, labelspacing=0.3)
-    plt.rc('xtick',  labelsize=22, direction='in', bottom='true', top='true')
-    plt.rc('ytick',  labelsize=22, direction='in', left='true', right='true')
     
-    
-    plt.rc('figure', figsize=[6, 6])
+    plt.rc('figure', figsize=[6/2.54, 6/2.54])
     plt.figure()
     plt.xlabel(r'Time (in $\tau_{relax}$ units)')
     plt.ylabel("Relative wave amplitude")
@@ -110,26 +132,184 @@ def plotComparison(Ohnumb, Bonumb, knumb, nbOfrelax):
     plt.plot(sampled_t,sampled_eta, 'black', label = r'Cortelezzi \& Prosperetti')
     plt.plot(sampled_t,sampled_eta_lub, 'gray', label = 'Lubricaion theory')
     plt.legend()
-    
-    
-    plt.rc('figure', figsize=[12, 6])
-    plt.figure()
+    plt.savefig('Oh_' + str(Ohnumb) + '_k_' + str(knumb) + '_eta.png')
+    plt.tight_layout(pad=0.)
+
+    plt.figure(figsize=[6./2.54, 6./2.54])
     plt.xlabel('Scaled vorticity')
     plt.ylabel('depth z')
-    plt.xlim([-1,7])
+    plt.xlim([-0.5, 2*len(sampled_omega[0])-0.5])
     plt.ylim([-1,0])
     plt.rc('legend', loc='best')
     ax = plt.gca()
     ax.axes.xaxis.set_ticks([])
+    plt.tight_layout(pad=0.)
     
-    for i in range(4):
-        plt.plot(sampled_omega[:,i], z_all, 'r', linewidth=1)
-        plt.plot(2*i*np.ones(100), z_all, 'r', linewidth=1)
+    for i in range(len(sampled_omega[0])):
+        plt.plot(sampled_omega[:,i], z_all, 'r')
+        plt.plot(2*i*np.ones(len(z_all)), z_all, 'r')
         plt.fill_betweenx(z_all, sampled_omega[:,i], 2*i, color = 'red', alpha=0.20)
         plt.plot(sampled_omega_lub[:,i],z_all, c = 'gray', linewidth=1, linestyle='dashed')
+        plt.text(2.*i+0.2, -0.1, str(toI[i]) + r'$\tau_{relax}$', size=8)
+
+    plt.savefig('Oh_' + str(Ohnumb) + '_k_' + str(knumb) + '_omg.png')
+
+
+#%%
+##Comparison with Lubrication
+plotComparison(10, 0.001, 0.1, 1)                   # oilly film
+plotComparison(0.01, 0.001, 0.1, 1)                 # waterborne coating
+plotComparison(0.005, 0.001, 0.1, 6, [0.3, 0.6])    # oscillations
+#%%
+##Feuilletage
+plotComparison(0.005, 0.001, 2., 2400, [1800, 2400])
+plotComparison(0.005, 0.001, 1.5, 1800, [1200, 1800])
+plotComparison(1e-4, 0.001, 2., 1./tau, [1./6.25e-6]) #tau = (Oh/k**4)
+#%% Feuilletage simplifié
+def Feuilletage(Ohnumb, Bonumb, knumb, nbOfrelax, toI = [0.05, 0.1]):
+
+    plt.rc('figure', figsize=[6/2.54, 6/2.54])   
+    z_all = np.linspace(-1, -0.999995, 1000)
+    sampled_omega, sampled_omega_lub = \
+        solveOmega(Ohnumb, Bonumb, knumb, nbOfrelax, toI, z_all)
+    
+    ### Figures ###
+    plt.xlabel('Scaled vorticity')
+    plt.ylabel('depth z')
+    plt.xlim([-0.5, 1.5])
+    plt.ylim([-1,-0.999995])
+
+    plt.plot(sampled_omega[:,0], z_all, 'r')
+    plt.fill_betweenx(z_all, sampled_omega[:,0], 0., color = 'red', alpha=0.20)
+    
+Oh = 1e-10
+k = 100.
+tau = (Oh/k**4)
+Feuilletage(Oh, 0.001, k, 5.e-3/tau, [5.25e-3/tau])
+ 
+sampled_t, sampled_eta, sampled_eta_lub = \
+    solveEta(0.01, -0.03, 0.05, 3.)
+plt.rc('figure')
+plt.rc('legend', loc='best')
+
+plt.figure()
+plt.xlabel(r'Time (in $\tau_{relax}$ units)')
+plt.ylabel("Relative wave amplitude")
+ #   plt.xlim([0,nbOfrelax])
+ #   plt.ylim([1,10.])
+plt.semilogy(sampled_t,sampled_eta, 'black', label = r'Cortelezzi \& Prosperetti')
+plt.semilogy(sampled_t,sampled_eta_lub, 'gray', label = 'Lubricaion theory')
+plt.legend()
+plt.tight_layout(pad=0.)
+
+ #%% Rayleigh-Taylor
+
+k_list = [0.001, 0.008, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.17, 0.173]
+om_list=[]
+om_lub_list =[]
+om_cort_lub = []
+for k in k_list:
+    Ohnumb, Bonumb, knumb, nbOfrelax, toI = 0.01, -0.03, k, 3., [3.]
+    
+    sampled_t, sampled_eta, sampled_eta_lub = \
+        solveEta(Ohnumb, Bonumb, knumb, nbOfrelax)
         
-    #%% 
-plotComparison(10, 0.001, 0.1, 1)    
-plotComparison(10, 0.001, 1., 1)    
-plotComparison(0.01, 0.001, 0.1, 1)    
-plotComparison(0.01, 0.001, 1., 250)
+    om, a0 = np.polyfit(sampled_t[20:], np.log(sampled_eta[20:]), 1)
+    
+    tau_relax = abs(float(3*(Ohnumb/(knumb**2*Bonumb+knumb**4))))
+    om_list.append(om/tau_relax)
+    om_lub_list.append(1./tau_relax)
+    om_cort_lub.append(np.sqrt(abs((Bonumb+knumb**2)*knumb*np.tanh(knumb))))
+    ### Figures ###
+    if True:
+        plt.rc('figure', figsize=[6/2.54, 6/2.54])
+        plt.figure()
+        plt.xlabel(r'Time (in $\tau_{relax}$ units)')
+        plt.ylabel("Relative wave amplitude")
+         #   plt.xlim([0,nbOfrelax])
+         #   plt.ylim([1,10.])
+        plt.rc('legend', loc='best')
+        
+        plt.semilogy(sampled_t,sampled_eta, 'black', label = r'Cortelezzi \& Prosperetti')
+#        plt.semilogy(sampled_t,sampled_eta_lub, 'gray', label = 'Lubricaion theory')
+        plt.semilogy(sampled_t,np.exp(a0 + sampled_t*om), 'gray', label = 'Lubricaion theory')
+        plt.legend()
+        plt.savefig('Oh_' + str(Ohnumb) + '_k_' + str(knumb) + '_eta.png')
+        plt.tight_layout(pad=0.)
+        
+        plt.savefig('Oh_' + str(Ohnumb) + '_k_' + str(knumb) + '_omg.png')
+
+
+  
+plt.figure()
+plt.xlabel(r'k')
+plt.ylabel(r'$\omega$')
+plt.plot(k_list, om_cort_lub, label = r'Potential')
+plt.plot(k_list, om_lub_list, label = r'Lubrication')
+plt.plot(k_list, om_list, label = r'Cortelezzi resolution')
+plt.legend()
+plt.tight_layout(pad=0.)
+
+#%% Rayleigh-Taylor : different periods
+knumb = 10**np.linspace(-3,0,1000)
+Ohnumb, Bonumb, nbOfrelax, toI = 0.01, -0.03, 3., [3.]
+
+tau_relax = np.abs(3*(Ohnumb/(knumb**2*Bonumb+knumb**4)))
+om_lub_list = (1./tau_relax)
+om_cort_lub = (np.sqrt(np.abs((Bonumb+knumb**2)*knumb*np.tanh(knumb))))
+
+plt.figure()
+plt.xlabel(r'k')
+plt.ylabel(r'$\omega$')
+plt.loglog(knumb, om_cort_lub, label = r'Potential')
+plt.loglog(knumb, om_lub_list, label = r'Lubrication')
+plt.plot(k_list, om_list, label = r'Cortelezzi resolution')
+plt.legend()
+
+#%% Rayleigh-Taylor old
+  
+# def plotGneg(Ohnumb, Bonumb, knumb, nbOfrelax, toI = [0.05, 0.1]):
+    
+#     sampled_t, sampled_eta, sampled_eta_lub = \
+#         solveEta(Ohnumb, Bonumb, knumb, nbOfrelax)
+    
+#     z_all = np.linspace(-1, 0, 200)
+#     sampled_omega, sampled_omega_lub = \
+#         solveOmega(Ohnumb, Bonumb, knumb, nbOfrelax, toI, z_all)
+    
+#     ### Figures ###
+    
+#     plt.rc('figure', figsize=[6/2.54, 6/2.54])
+#     plt.figure()
+#     plt.xlabel(r'Time (in $\tau_{relax}$ units)')
+#     plt.ylabel("Relative wave amplitude")
+#  #   plt.xlim([0,nbOfrelax])
+#  #   plt.ylim([1,10.])
+#     plt.rc('legend', loc='best')
+    
+#     plt.loglog(sampled_t,sampled_eta, 'black', label = r'Cortelezzi \& Prosperetti')
+#     plt.loglog(sampled_t,sampled_eta_lub, 'gray', label = 'Lubricaion theory')
+#     plt.legend()
+#     plt.savefig('Oh_' + str(Ohnumb) + '_k_' + str(knumb) + '_eta.png')
+#     plt.tight_layout(pad=0.)
+
+# #    plt.figure(figsize=[6./2.54, 6./2.54])
+# #    plt.xlabel('Scaled vorticity')
+# #    plt.ylabel('depth z')
+# #    plt.xlim([-0.5, 2*len(sampled_omega[0])-0.5])
+# #    plt.ylim([-1,0])
+# #    plt.rc('legend', loc='best')
+# #    ax = plt.gca()
+# #    ax.axes.xaxis.set_ticks([])
+# #    plt.tight_layout(pad=0.)
+    
+# #    for i in range(len(sampled_omega[0])):
+# #        plt.plot(sampled_omega[:,i], z_all, 'r')
+# #        plt.plot(2*i*np.ones(len(z_all)), z_all, 'r')
+# #        plt.fill_betweenx(z_all, sampled_omega[:,i], 2*i, color = 'red', alpha=0.20)
+# #        plt.plot(sampled_omega_lub[:,i],z_all, c = 'gray', linewidth=1, linestyle='dashed')
+# #        plt.text(2.*i+0.2, -0.1, str(toI[i]) + r'$\tau_{relax}$', size=8)
+
+#     plt.savefig('Oh_' + str(Ohnumb) + '_k_' + str(knumb) + '_omg.png')
+
+# plotGneg(0.01, -0.03, 0.1, 3., [3.])
